378d540df127f345f318e0584550914c0c672eff
Standupic <satndupic87@gmail.com>
2019-04-29 13:25:44 +0300

    send

---
+++ send.php
@@ -0,0 +1,19 @@
+<?
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+        $to = 'satndupic87@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
+        $subject = 'Заявка на приобретение франшизы песочно'; //Заголовок сообщения
+        $message = "
+                <html>
+                    <head>
+                        <title>".$subject."</title>
+                    </head>
+                    <body>
+                        <p>Имя: ".$_POST['name']."</p>
+                        <p>Телефон: ".$_POST['phone']."</p>";
+           $message .= "</body>
+                </html>"; //Текст нащего сообщения можно использовать HTML теги
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+        $headers .= "From: Франшиза песочно <cp75225@vh24.timeweb.ru>\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+}
+?>
