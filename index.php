b25b5473ef1fba41a4fc480f58667e54f4860fb6
Standupic <satndupic87@gmail.com>
2019-04-30 16:10:47 +0300

    a

---
+++ index.php
@@ -0,0 +1,481 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<title>Заголовок</title>
+	<meta name="description" content="">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no">
+	<link rel="stylesheet" href="css/_main.min.css">
+	<link rel="stylesheet" href="css/owl.carousel.css">
+	<link rel="stylesheet" href="css/owl.theme.default.css">
+	<link rel="stylesheet" href="css/cocoen.min.css">
+</head>
+<html>
+	<body>
+		<section class="header">
+			<div class="container">
+				<div class="top_header">
+					<div class="top_header_left">
+						<img class="mb_hidden menu_open" src="./img/Menu.png" alt="">
+						<img class="mb_hidden" src="./img/logo-pesochno.png" alt="">
+						<p class="franch mb_hidden ds_hidden">Франшиза детской <br/>выездной песочницы</p>
+						<img class="menu menu_open ds_hidden tb_hidden" src="./img/Menu.png" alt="">
+
+						<div class="ds_hidden tb_hidden top_header_left_mobile">
+							<img src="./img/logo-pesochno.png" alt="">
+							<p>Франшиза детской <br/>выездной песочницы</p>
+						</div>
+						
+					</div>
+					<div class="top_header_right">
+						<p class="call_me send">Перезвоните мне</p>
+						<div><a href="tel:+74997556584"><img src="./img/phone.png" alt=""></a></div>
+					</div>
+				</div><!--end top_header-->
+				<div class="content_header">
+					<div class="left_content_header">
+						<div class="top_left_content_header">
+							<h1>Откройте детскую <br class="tb_hidden ds_hidden"/>песочницу в <br class="mb_hidden" /> своем городе и <br class="tb_hidden ds_hidden"/> получайте доход<br class="mb_hidden"/> до <span>300 000 ₽</span></h1>
+							<p>Получите наш успешный опыт и <br class="ds_hidden tb_hidden"/> отлаженные бизнес-процессы</p>
+						</div>
+						<div class="bottom_left_content_header">
+							<div>
+								<button class="send">Отправить заявку</button>
+							</div>
+							<div class="video">
+								<img src="./img/Group5.png" alt="">
+								<p class="tb_hidden mb_hidden">Краткое видео о <br/> нашей работе</p>
+							</div>
+						</div>
+					</div>
+					<div class="right_content_header">
+						<img src="./img/Group6.png" class="info tb_hidden mb_hidden" alt="">
+						<img src="./img/pesok3.png" class="pesochno" alt="">
+					</div>
+				</div><!--end content_header-->
+			</div>
+			<div class="cycle mb_hidden tb_hidden">
+				<img src="./img/Oval.png" alt="">
+			</div>
+		</section>
+		<section class="what_is_it">
+			<div class="container">
+				<div class="header_what_is_it">
+					<h2>Что такое Песочно?</h2>
+					<p>Мы рисуем с детьми в торговых центрах, нас приглашают в детские сады и школы, на фестивали<br/> и дни рождения, что бы мы делали незабываемый праздник для детей!</p>
+				</div>
+				<div class="owl-carousel owl-one owl-theme">
+				  <div class="item">
+				  		<div class="wrap_items">
+				  			<div class="left">
+				  					<h3>Уникальное творческое увлечение для <br class="tb_hidden"/>детей</h3>
+				  					<p>Выезд на любой десткий праздник. Организация <br/>увлекательного процесса рисования<br class="tb_desktop tb_mobile"/> картин цветным песком.</p>
+				  			</div>
+				  			<div class="right">
+				  				<div class="cocoen">
+								    <img src="./img/before.png" alt="">
+								    <img src="./img/after.png" alt="">
+								</div>
+				  			</div>
+				  		</div>
+				  </div>
+				   <div class="item">
+				  		<div class="wrap_items">
+				  			<div class="left">
+				  					<h3>Уникальное творческое увлечение для <br class="tb_hidden"/>детей</h3>
+				  					<p>Выезд на любой десткий праздник. Организация <br/>увлекательного процесса рисования картин цветным песком.</p>
+				  			</div>
+				  			<div class="right">
+				  				<img src="./img/1.png" alt="">
+				  			</div>
+				  		</div>
+				  </div>
+				   <div class="item">
+				  		<div class="wrap_items">
+				  			<div class="left">
+				  					<h3>Удобное и легкое выездное <br class="tb_hidden"/>оборудование</h3>
+				  					<p>Стол, высотой 40см, шириной 60см, без острых<br/> углов. На рабочей зоне расположены контейнеры с <br/>цветным песком (от 10 до 12шт). Вместимость за <br/>один стол около 6-8 детей.</p>
+				  			</div>
+				  			<div class="right">
+				  				<img src="./img/2.png" alt="">
+				  			</div>
+				  		</div>
+				  </div>
+				</div>
+				<div class="counter"></div>
+			</div><!-- end container-->
+		</section>
+		<section class="advantages">
+			<div class="container">
+				<div class="header_advantages">
+					<h2>Преимущества нашей франшизы</h2>
+					<div class="wrap_items">
+						<div class="item">
+							<img src="./img/advantage_1.png" alt="">
+							<h3>Стартовый пакет товара</h3>
+				  			<p>трафареты и песок по розничной цене на <br/> 150 000 руб.</p>
+						</div>
+						<div class="item">
+							<img src="./img/advantage_2.png" alt="">
+							<h3>Нет прямых конкурентов</h3>
+							<p>мы единственная выездная песочница в <br/>вашем городе</p>
+						</div>
+						<div class="item">
+							<img src="./img/advantage_3.png" alt="">
+							<h3>Минимум рисков</h3>
+							<p>отработал - заработал, нет больших <br/> ежемесячных платежей по аренде</p>
+						</div>
+					</div>
+					<button class="send">Отправить заявку</button>
+				</div>
+			</div>
+		</section>
+		<section class="business_plan">
+			<div class="container">
+				<div class="wrap_business_plan">
+					<div class="contant">
+						<div class="left">
+							<div>
+								<img src="./img/girls_mobile.png" alt="">
+							</div>
+						</div>
+						<div class="right">
+							<h2>Получить подробный <br/> бизнес-план</h2>
+								<p>Оставьте заявку на франшизу и мы пришлем вам<br class="ds_hidden"/> подробный бизнес-план c <br class="tb_hidden" /> расчетом прибыли и <br class="ds_hidden"/>презентацию франшизы с ценами</p>
+								<form action="get_plan.php" id="get_plan_one">
+									<input type="text" placeholder="Ваше имя" name="name" required>
+									<input type="text" placeholder="Номер телефона" name="phone" class="phone" required>
+									<input type="text" placeholder="E-mail" name="email" required pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$">
+								<button type="submit">Отправить заявку</button>
+								</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="what_includes">
+			<div class="container">
+				<h2>Что входит в франшизу?</h2>
+				<div class="wrap_items">
+					<div class="item">
+						<img src="./img/colored-sand.png" alt="">
+						<h3>Стартовый пакет товара</h3>
+						<p>трафареты и песок по розничной цене на <br/> 200 руб.</p>
+					</div>
+					<div class="item">
+						<img src="./img/boxes.png" alt="">
+						<h3>Готовое оборудование</h3>
+						<p>2 стола, рекламные банеры, контейнеры, <br/> футболки для сотрудников и тгд</p>
+					</div>
+					<div class="item">
+						<img src="./img/instructure.png" alt="">
+						<h3>Инструкции</h3>
+						<p>подробные инструкции по запуску бизенса с <br/> описанием всех процессов</p>
+					</div>
+					<div class="item">
+						<img src="./img/brandbook.png" alt="">
+						<h3>Брендбук</h3>
+						<p>название, логотип, оборудование, все <br/> необходимые скрипты</p>
+					</div>
+					<div class="item">
+						<img src="./img/price.png" alt="">
+						<h3>Товар по выгодным ценам</h3>
+						<p>возможность заказать товар напрямую у <br/> производителя</p>
+					</div>
+					<div class="item">
+						<img src="./img/support.png" alt="">
+						<h3>Полная поддержка</h3>
+						<p>в первые два месяца поддержки и <br/> консультации по запуску бизнеса</p>
+					</div>
+				</div>
+				<button class="send">Отправить заявку</button>
+			</div>
+		</section>
+		<section class="about">
+			<div class="container">
+				<div class="wrap_items">
+					<div class="left">
+						<img src="./img/about.png" alt="">
+						<h3>Артем и Ирина Мишнев</h3>
+						<p>Основатели студии &laquo;Песочно&raquo;</p>
+					</div>
+					<div class="right">
+						<h2>О нас</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate aliquid aut hic sed, dignissimos totam ullam fugit, quos velit nobis impedit, assumenda eum harum quae porro. Consequuntur odit tempore, inventore. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, consequuntur iusto hic. Doloribus numquam distinctio odit, fugiat, atque quas repudiandae sit similique dolore impedit facilis officiis animi non ducimus labore. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias fuga accusamus dolore porro beatae voluptas dicta, consectetur impedit culpa itaque laborum harum exercitationem tempora, sint enim a sed eligendi doloribus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, expedita inventore a molestiae voluptas non iste rem. Quae soluta explicabo hic consequatur, possimus ullam. Consequuntur iure aperiam natus mollitia maiores!</p>
+						<button class="send">Отправить заявку</button>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="open">
+			<div class="container">
+				<h2>Открывайте свой бизнес и <br/> получите прибыль</h2>
+				<div class="menu">
+					<ul>
+						<li>
+							<p>1 этап</p>
+							<p>Бизнес план и <br/> знакомство</p>
+						</li>
+						<li>
+							<p>2 этап</p>
+							<p>Подписание <br/> договора</p>
+						</li>
+						<li>
+							<p>3 этап</p>
+							<p>Получение <br/> франшизы</p>
+						</li>
+						<li>
+							<p>4 этап</p>
+							<p>Обучение <br/> и подготовка</p>
+						</li>
+						<li>
+							<p>5 этап</p>
+							<p>Успешный <br/> запуск</p>
+						</li>
+					</ul>
+				</div>
+				<div class="contant">
+					<div class="wrap_items">
+						<div class="wrap_item">
+							<div class="item">
+								<div class="left first">
+									<h4>Бизнес план и знакомство</h4>
+									<p>Отправляете нам заявку и мы высылаем вам презентацию и подробную финансовую модель бизнеса для ознакомления. Мы связываемся с вами и консультируем по всем деталям проекта, отвечаем на все возникшие вопросы.</p>
+								</div>
+								<div class="right">
+									<img src="./img/open1.png" alt="">
+								</div>
+							</div>
+						</div>
+						<div class="wrap_item">
+							<div class="item">
+								<div class="left">
+									<h4>Подписание договора</h4>
+									<p>Оформление всех официальных документов, подписание договоров. Получение необходимых материалов.</p>
+								</div>
+								<div class="right">
+									<img src="./img/open2.png" alt="">
+								</div>
+							</div>
+						</div>
+						<div class="wrap_item">
+							<div class="item">
+								<div class="left">
+									<h4>Получение франшизы</h4>
+									<p>Оплата паушального взноса. Получение на руки всех необходимых инструкций и стартового пакета товара для начала работы.</p>
+								</div>
+								<div class="right">
+									<img src="./img/open3.png" alt="">
+								</div>
+							</div>
+						</div>
+						<div class="wrap_item">
+							<div class="item">
+								<div class="left">
+									<h4>Обучение и подготовка</h4>
+									<p>Оплата паушального взноса. Получение на руки всех необходимых инструкций и стартового пакета товара для начала работы.</p>
+								</div>
+								<div class="right">
+									<img src="./img/open4.png" alt="">
+								</div>
+							</div>
+						</div>
+						<div class="wrap_item">
+							<div class="item">
+								<div class="left">
+									<h4>Успешный запуск</h4>
+									<p>Оплата паушального взноса. Получение на руки всех необходимых инструкций и стартового пакета товара для начала работы.</p>
+								</div>
+								<div class="right">
+									<img src="./img/open5.png" alt="">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section>
+			<div class="container">
+				<button style="margin:40px auto" class="send">Отправить заявку</button>
+			</div>
+		</section>
+		<section class="price">
+			<div class="container">
+				<h2>Стоимость и расчет прибыли</h2>
+				<div class="wrap_items">
+					<div class="item">
+						<div class="left">
+							<h4>Стоимость франшизы:</h4>
+						</div>
+						<div class="right">
+							<p><span><s>200 000 ₽</s><em>180 000 ₽</em></span><span>цена франшизы при покупке до 31.05</span></p>
+						</div>
+					</div>
+					<div class="item">
+						<div class="left">
+							<h4>Прибыль:</h4>
+						</div>
+						<div class="right">
+							<p>Стоимость посещения музея составляет от 120 ₽* При выезде музея в школу его за день посещают около 150 чел. (5 классов по 30 чел.). Оборот в месяц: около 396 000 ₽* (за 22 рабочих дня).</p>
+						</div>
+					</div>
+					<div class="item">
+						<div class="left">
+							<h4>
+								Ежемесячные затраты:
+							</h4>
+						</div>
+						<div class="right">
+							<p>Зарплата (водитель-грузчик и консультант-ведущий) 40 000 ₽ + печатная рекламная продукция 5 000 ₽ + транспортные расходы 10 000 ₽ + прочие расходы 10 000 ₽ + роялти 11 880 ₽ (3% от оборота) + налоги 23 760 ₽ = 100 640 ₽*</p>
+						</div>
+					</div>
+					<div class="item">
+						<div class="left">
+							<h4>
+								Чистая прибыль:
+							</h4>
+						</div>
+						<div class="right">
+							<p><span>396 000 ₽ - 100 640 ₽ = <em>295 360 ₽&#42; в мес.</em></span></p>
+						</div>
+					</div>
+				</div>
+				<button class="send">Отправить заявку</button>
+				<p class="info">* Расчеты составлены по данным управляющей компании. Успешность бизнеса зависит от вас.</p>
+			</div>
+		</section>
+		<section class="faq">
+			<div class="container">
+				<h2>У вас отсались вопросы?</h2>
+				<div class="wrap_items">
+					<div class="item">
+						<h3>Я никогда не занимался бизнесом, смогу ли быстро во всем разобраться?</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem excepturi praesentium, deserunt eos esse tempora repudiandae commodi, quas dolores et soluta ipsa placeat dolorem odio molestias id dolore eveniet odit!</p>
+					</div>
+					<div class="item">
+						<h3>Я никогда не занимался бизнесом, смогу ли быстро во всем разобраться?</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem excepturi praesentium, deserunt eos esse tempora repudiandae commodi, quas dolores et soluta ipsa placeat dolorem odio molestias id dolore eveniet odit!</p>
+					</div>
+					<div class="item">
+						<h3>Я никогда не занимался бизнесом, смогу ли быстро во всем разобраться?</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem excepturi praesentium, deserunt eos esse tempora repudiandae commodi, quas dolores et soluta ipsa placeat dolorem odio molestias id dolore eveniet odit!</p>
+					</div>
+					<div class="item">
+						<h3>Я никогда не занимался бизнесом, смогу ли быстро во всем разобраться?</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem excepturi praesentium, deserunt eos esse tempora repudiandae commodi, quas dolores et soluta ipsa placeat dolorem odio molestias id dolore eveniet odit!</p>
+					</div>
+					<div class="item">
+						<h3>Я никогда не занимался бизнесом, смогу ли быстро во всем разобраться?</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem excepturi praesentium, deserunt eos esse tempora repudiandae commodi, quas dolores et soluta ipsa placeat dolorem odio molestias id dolore eveniet odit!</p>
+					</div>
+				</div>
+				<button class="send">Отправить заявку</button>
+				<p>Не нашли ответ на свой вопрос? Закижите обратный звонок и мы<br/> с радостью ответим на все вопросы.</p>
+			</div>	
+		</section>
+		<section class="foto">
+			<div class="owl-carousel owl-two owl-theme" id="owl-two">
+			    <div class="item"><img src="./img/foto2.png" alt=""></div>
+			    <div class="item"><img src="./img/foto3.png" alt=""></div>
+			    <div class="item"><img src="./img/foto4.png" alt=""></div>
+			    <div class="item"><img src="./img/foto1.png" alt=""></div>
+			</div>
+		</section>
+		<section class="get_plan">
+			<div class="container">
+				<div class="wrap_business_plan">
+					<div class="contant">
+							<div class="left">
+								<img src="./img/get_plan_bg.png" alt=""/>
+							</div>
+							<div class="right">
+								<h2>Получить подробный <br/> бизнес-план</h2>
+								<p>Оставьте заявку на франшизу и мы пришлем вам<br class="ds_hidden"/> подробный бизнес-план c <br class="tb_hidden" /> расчетом прибыли и <br class="ds_hidden"/>презентацию франшизы с ценами
+								</p>
+									<form action="get_plan.php" id="get_plan_two">
+										<input type="text" placeholder="Ваше имя" name="name" required>
+										<input type="text" placeholder="Номер телефона" name="phone" class="phone" required>
+										<input type="text" placeholder="E-mail" name="email" required pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$">
+									<button type="submit">Отправить заявку</button>
+									</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			<img src="./img/oval_get.png" class="oval" alt="">
+		</section>
+		<div class="footer">
+			<div class="container">
+				<div class="wrap_footer">
+					<div class="left">
+						<img src="./img/logo-pesochno-w.png" alt="">
+						<p>Франшиза детской <br/>выездной песочницы</p>
+					</div>
+					<div class="right">
+						<p class="send">Перезвоните мне</p>
+						<div><a href="#"><img src="./img/phone.png" alt=""></a></div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="wrap_pop_up">
+			<div class="screen1">
+				<form id="form">
+					<div class="pop_up">
+						<div>
+							<img src="./img/close.png" alt="" class="pop_up_close">
+						</div>
+						<h5>Укажите контактные данные и мы <br/> перезвоним вам в ближайшее время</h5>
+						<input type="text" name="name" placeholder="Ваше имя" required>
+						<div class="error"></div>
+						<input type="text" name="phone" class="phone" id="pop_up" placeholder="Номер телефона" required>
+						<div class="error"></div>
+						<button type="submit" class="send">Заказать звонок</button>
+					</div>
+				</form>
+			</div>
+			<div class="screen2">
+				<div class="pop_up">
+					<div>
+						<img src="./img/close.png" class="pop_up_close" alt="">
+					</div>
+					<h5>Спасибо за заявку! Мы свяжемся с вами в ближайшее время.</h5>
+					<button class="pop_up_close">Закрыть</button>
+				</div>
+			</div>
+			<div class="screen3">
+				<div class="pop_up">
+					<div>
+						<img src="./img/close.png" class="pop_up_close" alt="">
+					</div>
+					<h5>К сожалению ваша заявка не отправилась. Возможно это связано с техническими проблемами если очередные попытки будут не удачными, пожалуйста позвоните нам. <a href="tel:+74997556584">+7(495)7556584</a></h5>
+					<button class="pop_up_close">Закрыть</button>
+				</div>
+			</div>	
+		</div>
+		<div class="sidemenu">
+			<img src="./img/close.png" class="close_sidemenu" alt="">
+			<ul>
+				<li><a href="#">Что такое песочно</a></li>
+				<li><a href="#">Преимущества</a></li>
+				<li><a href="#">Получить бизнес-план</a></li>
+				<li><a href="#">Что входит</a></li>
+				<li><a href="#">О нас</a></li>
+				<li><a href="#">Этапы запуска</a></li>
+				<li><a href="#">Расчет прибыли</a></li>
+				<li><a href="#">Ответы на вопросы</a></li>
+				<li><a href="#">Фото</a></li>
+			</ul>
+			<p>Оставьте заявку и мы пришлем вам подробный бизнес-план</p>
+			<button class="send_from_menu">Отправить заявку</button>
+		</div>
+	</body>
+	<script src="libs/jquery-1.11.2.min.js"></script>
+	<script src="js/owl.carousel.js"></script>
+	<script src="js/mask.js"></script>
+	<script src="js/cocoen.min.js"></script>
+	<script src="js/cocoen-jquery.min.js"></script>
+	<script src="js/common.min.js"></script>
+</html>
+
