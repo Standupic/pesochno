596f51aaeeabe82357922628d07a1c5ca95723eb
Standupic <satndupic87@gmail.com>
2019-04-30 13:17:53 +0300

    x

--- get_plan.php
+++ get_plan.php
@@ -3,22 +3,7 @@
 	  (isset($_POST['phone'])&&$_POST['phone']!="") &&
 	  (isset($_POST['email'])&&$_POST['email']!="")){
 	  $to = $_POST['email']; //Почта получателя, через запятую можно указать сколько угодно адресов
-	  $to2 = "satndupic87@gmail.com,pesochnofranchise@gmail.com";
-        $subject = "Бизнес план франшизы песочно"; //Заголовок сообщения
-        $subject2 = "Кто то запросил бизнес план франшизы песочно";
-        $message2 =  "<html>
-        				<head>
-        					<title>".$subject2."</title>
-        				</head>
-        				<body>
-        					<p>
-        						Артем, до ношу до Вашего сведения что некто".$_POST['name']." запросил бизнес план твоей франшизы.
-        					</p>
-        					<p>Контактные данные:</p>
-        					<p>Телефон: ".$_POST['phone']."</p>
-        					<p>Почта: ".$_POST['email']."</p>
-        				</body>
-        			</html>";
+        $subject = 'Бизнес план франшизы песочно'; //Заголовок сообщения
         $message = "
                 <html>
                     <head>
@@ -30,7 +15,6 @@
                 </html>"; //Текст нащего сообщения можно использовать HTML теги
         $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
         $headers .= "From: Франшиза песочно <cp75225@vh24.timeweb.ru>\r\n"; //Наименование и почта отправителя
-        mail($to, $subject, $message, $headers);
-        // mail($to2, $subject2, $message2, $headers);
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
 	}
 ?>
