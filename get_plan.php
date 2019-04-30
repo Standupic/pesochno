6b18cdeae63061a326f31eaf5d4b6ac53463e181
Standupic <satndupic87@gmail.com>
2019-04-30 14:36:17 +0300

    a

--- get_plan.php
+++ get_plan.php
@@ -27,9 +27,9 @@
                         <title>".$subject."</title>
                     </head>
                     <body>
-                        <p>Ссылка на скачивание Бизнес плана франшизы 'Песочно' <a href='https://yadi.sk/i/qoLR7hdi-J9Y2g'>Бизнес план</a></p>";
-           $message .= "</body>
-                </html>"; //Текст нащего сообщения можно использовать HTML теги
+                        <p>Ссылка на скачивание Бизнес плана франшизы 'Песочно' <a href='https://yadi.sk/i/qoLR7hdi-J9Y2g'>Бизнес план</a></p>
+                    </body>
+                </html>";
         $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
         $headers .= "From: Франшиза песочно <cp75225@vh24.timeweb.ru>\r\n"; //Наименование и почта отправителя
         mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
